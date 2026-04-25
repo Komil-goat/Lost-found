75a182250a3a148e8d55b14baea8dadd4c93840f
Komil Qodirov <komilkadirov340@gmail.com>
2026-04-25 15:52:04 +0500

    added style and made number unique

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        # Add any extra fields you put in your model here
-        fields = UserCreationForm.Meta.fields + ("phone_number",)
+        fields = UserCreationForm.Meta.fields + ('phone_number',)
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if CustomUser.objects.filter(phone_number=phone_number).exists():
+            raise ValidationError("A user with this phone number already exists.")
+        return phone_number
