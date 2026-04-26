9c04bc2c13475ab2e3a4dd0e0bf447bfb0089003
oppoqqor <oppoqqor196985@gmail.com>
2026-04-26 12:50:41 +0500

    finished the lost/found items part. added two buttons on main page. added login_required for items views.

--- items/views.py
+++ items/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render, redirect
 from .models import ItemPost
-from .forms import FoundItemForm
+from .forms import FoundItemForm, LostItemForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
+def item_list(request, post_type):
+    items = ItemPost.objects.filter(post_type=post_type, resolved=False)
+    return render(request, 'items/list_item.html', {
+        'items': items,
+        'post_type': post_type
+    })
 
-def found_items_list(request):
-    items = ItemPost.objects.filter(post_type='found', resolved=False)
-    return render(request, 'items/found_list.html', {'items': items})
+@login_required
+def create_item(request, post_type):
+    form_class = FoundItemForm if post_type == 'found' else LostItemForm
 
-
-def create_found_item(request):
     if request.method == 'POST':
-        form = FoundItemForm(request.POST, request.FILES)
+        form = form_class(request.POST, request.FILES)
         if form.is_valid():
             item = form.save(commit=False)
-            item.post_type = 'found'
+            item.post_type = post_type
             item.user = request.user
             item.save()
-            return redirect('found_list')
+            return redirect(f'{post_type}_list')
     else:
-        form = FoundItemForm()
+        form = form_class()
 
-    return render(request, 'items/create_found.html', {'form': form})
+    return render(request, 'items/create_item.html', {
+        'form': form,
+        'post_type': post_type
+    })
