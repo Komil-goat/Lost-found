4fa351d0ef6ee0845fd6084a71ce4c326f32668c
greenlght <khaydarov1309@gmail.com>
2026-04-28 18:36:30 +0500

    add Tag model and connect it to ItemPost in items/models.py

--- items/models.py
+++ items/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.conf import settings
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class ItemPost(models.Model):
     TYPE_CHOICES = [
@@ -17,6 +22,8 @@ class ItemPost(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     claimed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return f"{self.title} ({self.post_type})"
+    
