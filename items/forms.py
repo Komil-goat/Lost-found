b93aa2b868af9f729aac451c03241ba91802fec4
greenlght <khaydarov1309@gmail.com>
2026-04-29 17:04:35 +0500

    fix conflict

--- items/forms.py
+++ items/forms.py
@@ -2,38 +2,11 @@ from django import forms
 from .models import ItemPost
 
 
-class FoundItemForm(forms.ModelForm):
+class ItemForm(forms.ModelForm):
     class Meta:
         model = ItemPost
         fields = ['title', 'description', 'location', 'date', 'image', 'tags']
-
-        widgets = {
-            'date': forms.DateInput(attrs={
-                'type': 'date'
-            }),
-            'tags': forms.CheckboxSelectMultiple(),
-        }
-
-    def clean_date(self):
-        date = self.cleaned_data.get('date')
-
-        if not date:
-            raise forms.ValidationError("Date is required")
-
-        return date
-
-class LostItemForm(forms.ModelForm):
-    class Meta:
-        model = ItemPost
-        fields = ['title', 'description', 'location', 'date', 'image', 'tags']
-
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
-            'tags': forms.CheckboxSelectMultiple(),
+            'tags': forms.CheckboxSelectMultiple()
         }
-
-    def clean_date(self):
-        date = self.cleaned_data.get('date')
-        if not date:
-            raise forms.ValidationError("Date is required")
-        return date
