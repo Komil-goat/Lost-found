f4df9dda3006cf449dbe89149954df83a915fac4
oppoqqor <oppoqqor196985@gmail.com>
2026-04-29 12:33:29 +0500

    reversed items

--- items/forms.py
+++ items/forms.py
@@ -2,36 +2,10 @@ from django import forms
 from .models import ItemPost
 
 
-class FoundItemForm(forms.ModelForm):
+class ItemForm(forms.ModelForm):
     class Meta:
         model = ItemPost
         fields = ['title', 'description', 'location', 'date', 'image']
-
-        widgets = {
-            'date': forms.DateInput(attrs={
-                'type': 'date'
-            })
-        }
-
-    def clean_date(self):
-        date = self.cleaned_data.get('date')
-
-        if not date:
-            raise forms.ValidationError("Date is required")
-
-        return date
-
-class LostItemForm(forms.ModelForm):
-    class Meta:
-        model = ItemPost
-        fields = ['title', 'description', 'location', 'date', 'image']
-
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'})
         }
-
-    def clean_date(self):
-        date = self.cleaned_data.get('date')
-        if not date:
-            raise forms.ValidationError("Date is required")
-        return date
