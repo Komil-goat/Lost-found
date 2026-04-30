b477d2cd75b745d63c6314cf431a08a406e3e428
Komil Qodirov <komilkadirov340@gmail.com>
2026-04-30 17:39:06 +0500

    Profile page added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from .forms import ProfileUpdateForm 
+from items.models import ItemPost
 
 def register(request):
     if request.method == 'POST':
@@ -12,3 +15,21 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    user_posts = ItemPost.objects.filter(user=request.user).order_by('-created_at')
+
+    context = {
+        'form': form,
+        'user_posts': user_posts
+    }
+    return render(request, 'users/profile.html', context)
