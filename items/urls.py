1bd89dc972e967caa07042c43212abaf28213c4b
greenlght <khaydarov1309@gmail.com>
2026-05-03 01:10:44 +0500

    add match path and suggestions div into create_item.html

--- items/urls.py
+++ items/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('<int:item_id>/edit/', views.edit_item, name='edit_item'),
     path('<int:item_id>/delete/', views.delete_item, name='delete_item'),
     path('item/<int:item_id>/', views.item_detail, name='item_detail'),
+
+    path('match/', views.match_items, name='match_items'),
 ]
